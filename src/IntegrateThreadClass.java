ee73cf694ccf85bc7097a58f7cf283e7ea079594
Andrew716 <andriikoropets@gmail.com>
2015-03-15 13:20:51 +0200

    made changes1

--- src/IntegrateThreadClass.java
+++ src/IntegrateThreadClass.java
@@ -5,22 +5,27 @@ public class IntegrateThreadClass extends Thread {
 
     private double LEFT_LIMIT;
     private double RIGHT_LIMIT;
-    public double localSum_1;
-    public double localSum_2;
+    private double localSum_1;
+    private double localSum_2;
     public static final double STEP_1 = 0.001;
     public static final double STEP_2 = 0.0001;
-
-
-
 
     public double function(double x ){
 
         return Math.pow(x,3)/Math.pow( Math.pow(x,2)+9,3/2);
-}
+    }
 
     public static double initial(double x){
 
         return Math.pow(Math.pow(x,2)+9,1/2)+ 9/Math.pow(Math.pow(x,2)+9,1/2);
+    }
+
+    public double getLocalSum_2() {
+        return localSum_2;
+    }
+
+    public double getLocalSum_1() {
+        return localSum_1;
     }
 
     public IntegrateThreadClass(double LEFT_LIMIT, double RIGHT_LIMIT) {
@@ -28,21 +33,13 @@ public class IntegrateThreadClass extends Thread {
         this.RIGHT_LIMIT = RIGHT_LIMIT;
     }
 
-
     @Override
     public void run(){
-
-
         for (double i = LEFT_LIMIT; i<RIGHT_LIMIT; i+=STEP_1){
-
             localSum_1 += function(i)*STEP_1;
         }
-
         for (double i = LEFT_LIMIT; i<RIGHT_LIMIT; i+=STEP_2){
-
             localSum_2 += function(i)*STEP_2;
         }
     }
-
-
 }
--- src/MainClass.java
+++ src/MainClass.java
@@ -4,10 +4,7 @@ import java.util.List;
 /**
  * Created by Andrii on 2/24/2015.
  */
-
-
 public class MainClass {
-
     public static double sum1 = 0;
     public static double sum2 = 0;
     public static double LEFT_LIMIT = 2;
@@ -16,28 +13,19 @@ public class MainClass {
     private static double checkOut;
 
     public static void main(String [] args) throws Exception {
-
-
         List<IntegrateThreadClass> threadList = new ArrayList<IntegrateThreadClass>();
-
         for (int i = 0; i< threadCount; i++){
-
             threadList.add(new IntegrateThreadClass(LEFT_LIMIT + (RIGHT_LIMIT - LEFT_LIMIT)/threadCount*i,LEFT_LIMIT + (RIGHT_LIMIT -LEFT_LIMIT)/threadCount*(i+1)));
             threadList.get(i).start();
         }
-
         for (int i = 0; i< threadList.size(); i++){
-
             threadList.get(i).join();
-            sum1 +=threadList.get(i).localSum_1;
-            sum2 +=threadList.get(i).localSum_2;
+            sum1 +=threadList.get(i).getLocalSum_1();
+            sum2 +=threadList.get(i).getLocalSum_2();
         }
-
         System.out.println("Result with the step_0.001 =  "+sum1);
         System.out.println("Result with the step_0.0001 =  "+sum2);
         checkOut = IntegrateThreadClass.initial(RIGHT_LIMIT) - IntegrateThreadClass.initial(LEFT_LIMIT);
-
         System.out.println("Result of initial =   "+checkOut);
-
     }
 }
